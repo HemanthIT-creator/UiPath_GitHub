b6e01e6e67a4543c5bf92dc2b15aff89e3720d58
hmacha <hmacha@planittesting.com>
2026-08-20 12:39:39 +0530

    Changes

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace Ui_BankTestAutomationProject
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
